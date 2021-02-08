c79eabb8fb219c22d6bfd9cace517f0ae38343e3
Kenneth <kethlastname@gmail.com>
2021-02-07 21:07:16 -0700

    Change the display on console, adding the colors and implement a function to get a list of paths

--- coolpath.py
+++ coolpath.py
@@ -4,40 +4,67 @@ import os
 # PATH = The environment variable
 # path = A directory path
 
+# Colors for console displaying
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 
 # Getting the PATH variable
 def getPATH():
-	STR_PATH = os.environ.get('PATH')
-	return STR_PATH
+	PATH = os.environ.get('PATH')
+	return PATH
+
+# This function divide each path in the PATH variable and store it in another list. crazy no?
+def listOfPaths(PATH):
+
+	listOfPaths = list(PATH) # Convert the String PATH to a list PATH
+
+	newList = [[]] # Inside this list will be the paths.
+
+
+	# Sincerly I don't even know how I made the following, so I hope you understand me in my next comments sorry.
+
+	# This for is to divide the paths inside the listOfPaths into another lists inside this
+	counterA = 0
+	for letter in listOfPaths:
+		# Accordingly to the ":" found, will increase the counter by 1 making separate lists
+		if letter != ':':
+			newList[counterA].append(letter)
+		else:
+			newList.append([])
+			counterA += 1
+
+	# This for convert the lists inside listOfPaths into strings 
+	counterB = 0
+	for lista in newList: # Yes, lista means list in spanish, list is a reserved keyword so i don't want problems :P
+		newList[counterB] = ''.join(lista)
+		counterB += 1 
+
+	return newList
+
 
 # Function to display a cool view of the path
-def displayPathListForm(STR_PATH):
+def listView(PATH):
 
-	# Converting the PATH string into a list to modify it
-	PATH = list(STR_PATH)
-
-	# Counter for the numbered list, and insertion of the first path in the PATH with the number '1'
+	# For the moment this print the paths with a header number before each one :)
 	counter = 1
-	PATH.insert(0, str(counter) + ': ')
+	for path in PATH:
+		print(bcolors.HEADER + str(counter) + ": " + bcolors.ENDC + path)
+		counter += 1
 
-	# Cheking each letter in the PATH
-	for letter in range(0, len(PATH)):
-
-		# Looking for ':' to change it for '\n'
-		if PATH[letter] == ':':
-			counter = counter + 1 # Taiming the counter for the numbered list
-
-			PATH[letter] = '\n' # Changing the ':' for '\n'
-
-			PATH.insert(letter + 1, str(counter) + ': ') # Inserting the number order
-
-	PATH = ''.join(PATH) # Reverting the list into a string
-
-	print(PATH) # Printing the PATH with the changes made
 
 # Main function of the program
 def main():
-	STR_PATH = getPATH()
-	displayPathListForm(STR_PATH)
+	listView(PATH)
 
+
+PATH = listOfPaths(getPATH())
 main()
