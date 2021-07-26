218db982cff31e12de1880ae2ffe96350c4f1a28
Kenneth Quintero <kennethgqv@gmail.com>
2021-07-26 16:42:21 -0600

    New initial version of coolpath with docopt

--- coolpath.py
+++ coolpath.py
@@ -1,70 +1,40 @@
+"""CoolPath.
+
+Usage:
+  coolpath list
+  coolpath add <path>...
+  coolpath remove <path>...
+  coolpath (-h | --help)
+  coolpath --version
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+
+"""
+from docopt import docopt
 import os
 
-# Clarifying things:
-# PATH = The environment variable
-# path = A directory path
+PATH = os.environ.get('PATH')
 
-# Colors for console displaying
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+def showPath():
+  print(PATH)
 
+def add(path):
+  print('add executed, {0} added to the path'.format(path))
 
-# Getting the PATH variable
-def getPATH():
-	PATH = os.environ.get('PATH')
-	return PATH
+def remove(path):
+  print('remove executed, {0} removed from path'.format(path))
 
-# This function divide each path in the PATH variable and store it into another list. crazy no?
-def listOfPaths(PATH):
+if __name__ == '__main__':
+  arguments = docopt(__doc__, version='CoolPath v1.0')
 
-	listOfPaths = list(PATH) # Convert the String PATH to a list PATH
+  if arguments['list']:
+    showPath()
+  elif arguments['add']:
+    add(arguments['<path>'])
+  elif arguments['remove']:
+    remove(arguments['<path>'])
 
-	newList = [[]] # Inside this list will be the paths.
-
-
-	# Sincerly I don't even know how I made the following, so I hope you understand me in my next comments sorry.
-
-	# This for is to divide the paths inside the listOfPaths into another lists inside this
-	counterA = 0
-	for letter in listOfPaths:
-		# Accordingly to the ":" found, will increase the counter by 1 making separate lists
-		if letter != ':':
-			newList[counterA].append(letter)
-		else:
-			newList.append([])
-			counterA += 1
-
-	# This for convert the lists inside listOfPaths into strings 
-	counterB = 0
-	for lista in newList: # Yes, lista means list in spanish, list is a reserved keyword so i don't want problems :P
-		newList[counterB] = ''.join(lista)
-		counterB += 1 
-
-	return newList
-
-
-# Function to display a cool view of the path
-def listView(PATH):
-
-	# For the moment this print the paths with a header number before each one :)
-	counter = 1
-	for path in PATH:
-		print(bcolors.HEADER + str(counter) + ": " + bcolors.ENDC + path)
-		counter += 1
-
-
-# Main function of the program
-def main():
-	listView(PATH)
-
-
-PATH = listOfPaths(getPATH())
-main()
+  # for debugging
+  # print(arguments)
